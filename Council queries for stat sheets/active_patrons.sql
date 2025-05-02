03cdb3275f030850b1d4167b3f44a402037c12ce
Brandon Cole <brandon@marmot.org>
2025-05-02 14:53:13 -0600

    Added active patrons query

---
+++ Council queries for stat sheets/active_patrons.sql
@@ -0,0 +1,11 @@
+SELECT
+	apn."name",
+	COUNT(pv.id)
+FROM
+	sierra_view.patron_view AS pv
+	JOIN sierra_view.agency_property AS ap ON ap.code_num = pv.patron_agency_code_num
+	JOIN sierra_view.agency_property_name AS apn ON apn.agency_property_id = ap.id
+WHERE
+	pv.activity_gmt >= '2022-05-01'
+GROUP BY
+	apn."name"
